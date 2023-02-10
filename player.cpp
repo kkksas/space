cbda927479a3b21202fa1da07220da0ec8ca2db2
Denis Piyalkin <pialkind@mail.ru>
2023-02-10 22:06:31 +0300

    Added update & control funcionality to player.cpp

--- player.cpp
+++ player.cpp
@@ -15,7 +15,24 @@ Player::~Player(){
     }
 }
 void Player::control(){
-    
+    if (Keyboard::isKeyPressed(Keyboard::Left)) {
+        if(!(x+BASE_SIZE/2<0)){
+        state = left;
+        speed = 0.1;}
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Right) ) {
+        if(!(x+BASE_SIZE/2>SCREEN_WIDTH)){
+            state = right;
+            speed = 0.1;
+        }
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Up)) {
+        if (reload_timer.getElapsedTime().asMilliseconds() > RELOAD_TIME){
+            Bullet* tmp_bullet= new Bullet(bullet_img,x,y-BASE_SIZE,BASE_SIZE,BASE_SIZE,window,-0.2);
+            bullet_list.push_back(tmp_bullet);
+            reload_timer.restart();
+        }
+    }
 }
 void Player::reset(bool win){
     x=0;
@@ -34,7 +51,43 @@ void Player::die(){
 }
 void Player::update(float time) //метод "оживлени¤/обновлени¤" объекта класса.
 {
-
+if (life) {//провер¤ем, жив ли герой
+    control();//функци¤ управлени¤ персонажем
+    switch (state)//делаютс¤ различные действи¤ в зависимости от состо¤ни¤
+    {
+    case right:{
+        dx = speed;
+        break;
+    }
+    case left:{
+        dx = -speed;
+        break;
+    }
+    case stay:{//стоим
+        dy = speed;
+        dx = speed;
+        break;
+    }
+    }
+    x += dx*time; //движение по УXФ
+    speed = 0; //обнул¤ем скорость, чтобы персонаж остановилс¤..
+    sprite.setPosition(x, y);
+    window->draw(sprite);
+    iter = bullet_list.begin();
+    while (iter != bullet_list.end()){
+        (*iter)->update(time);
+        if((*iter)->get_y()<-BASE_SIZE || (*iter)->get_life()==false){
+            if ((*iter)->get_life()==false)
+                score+=ENEMY_VALUE;
+            bullet_list.erase(iter);
+        }
+        *iter++;
+    }
+    for (iter=enemy_bullets->begin();iter!=enemy_bullets->end();iter++){
+        if (get_hitbox().intersects((*iter)->get_hitbox())){
+            die();
+        }
+    }
 }
 }
 list<Bullet*>* Player::get_player_bullet_list(){
